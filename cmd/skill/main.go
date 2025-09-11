d8971561312a52c0c175f5a8fd6960235f5b6645
Sergei Kotkin <sorrowful.free@gmail.com>
2025-09-11 15:54:55 +0300

    - added alice ed 9

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -32,33 +34,32 @@ func run() error {
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
+	//...
 
-	// десериализуем запрос в структуру модели
-	logger.Log.Debug("decoding request")
-	var req models.Request
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&req); err != nil {
-		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	text := "Для вас нет новых сообщений."
 
-	// проверяем, что пришёл запрос понятного типа
-	if req.Request.Type != models.TypeSimpleUtterance {
-		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
+	// первый запрос новой сессии
+	if req.Session.New {
+		// обрабатываем поле Timezone запроса
+		tz, err := time.LoadLocation(req.Timezone)
+		if err != nil {
+			logger.Log.Debug("cannot parse timezone")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// получаем текущее время в часовом поясе пользователя
+		now := time.Now().In(tz)
+		hour, minute, _ := now.Clock()
+
+		// формируем текст ответа
+		text = fmt.Sprintf("Точное время %d часов, %d минут. %s", hour, minute, text)
 	}
 
 	// заполняем модель ответа
 	resp := models.Response{
 		Response: models.ResponsePayload{
-			Text: "Извините, я пока ничего не умею",
+			Text: text, // Алиса проговорит новый текст
 		},
 		Version: "1.0",
 	}
--- cmd/skill/main_test.go
+++ cmd/skill/main_test.go
@@ -14,49 +14,76 @@ import (
 )
 
 func TestWebhook(t *testing.T) {
-	// тип http.HandlerFunc реализует интерфейс http.Handler
-	// это поможет передать хендлер тестовому серверу
 	handler := http.HandlerFunc(webhook)
-	// запускаем тестовый сервер, будет выбран первый свободный порт
 	srv := httptest.NewServer(handler)
-	// останавливаем сервер после завершения теста
 	defer srv.Close()
 
-	// ожидаемое содержимое тела ответа при успешном запросе
-	successBody := `{
-        "response": {
-            "text": "Извините, я пока ничего не умею"
-        },
-        "version": "1.0"
-    }`
-
-	// описываем набор данных: метод запроса, ожидаемый код ответа, ожидаемое тело
 	testCases := []struct {
+		name         string
 		method       string
+		body         string
 		expectedCode int
 		expectedBody string
 	}{
-		{method: http.MethodGet, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPut, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodDelete, expectedCode: http.StatusMethodNotAllowed, expectedBody: ""},
-		{method: http.MethodPost, expectedCode: http.StatusOK, expectedBody: successBody},
+		{
+			name:         "method_get",
+			method:       http.MethodGet,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_put",
+			method:       http.MethodPut,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_delete",
+			method:       http.MethodDelete,
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_without_body",
+			method:       http.MethodPost,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_unsupported_type",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "idunno", "command": "do something"}, "version": "1.0"}`,
+			expectedCode: http.StatusUnprocessableEntity,
+			expectedBody: "",
+		},
+		{
+			name:         "method_post_success",
+			method:       http.MethodPost,
+			body:         `{"request": {"type": "SimpleUtterance", "command": "sudo do something"}, "session": {"new": true}, "version": "1.0"}`,
+			expectedCode: http.StatusOK,
+			// ответ стал сложнее, поэтому сравниваем его с шаблоном вместо точной строки
+			expectedBody: `Точное время .* часов, .* минут. Для вас нет новых сообщений.`,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.method, func(t *testing.T) {
-			// делаем запрос с помощью библиотеки resty к адресу запущенного сервера,
-			// который хранится в поле URL соответствующей структуры
 			req := resty.New().R()
 			req.Method = tc.method
 			req.URL = srv.URL
+
+			if len(tc.body) > 0 {
+				req.SetHeader("Content-Type", "application/json")
+				req.SetBody(tc.body)
+			}
 
 			resp, err := req.Send()
 			assert.NoError(t, err, "error making HTTP request")
 
 			assert.Equal(t, tc.expectedCode, resp.StatusCode(), "Response code didn't match expected")
-			// проверяем корректность полученного тела ответа, если мы его ожидаем
 			if tc.expectedBody != "" {
-				assert.JSONEq(t, tc.expectedBody, string(resp.Body()))
+				// сравниваем тело ответа с ожидаемым шаблоном
+				assert.Regexp(t, tc.expectedBody, string(resp.Body()))
 			}
 		})
 	}
@@ -79,7 +106,7 @@ func TestGzipCompression(t *testing.T) {
 	// ожидаемое содержимое тела ответа при успешном запросе
 	successBody := `{
         "response": {
-            "text": "Извините, я пока ничего не умею"
+            "text": "Для вас нет новых сообщений."
         },
         "version": "1.0"
     }`
