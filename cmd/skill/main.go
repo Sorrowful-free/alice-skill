2de85912e9cab0954699e7b300848d9bb15c48e0
Sergei Kotkin <sorrowful.free@gmail.com>
2025-09-16 22:49:27 +0300

    - alice ed 11

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,17 +1,17 @@
-// пакеты исполняемых приложений должны называться main
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"strings"
 
-	"go.uber.org/zap"
-
 	"github.com/Sorrowful-free/alice-skill/internal/logger"
+	"github.com/Sorrowful-free/alice-skill/internal/store/pg"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"go.uber.org/zap"
 )
 
 func main() {
-	// обрабатываем аргументы командной строки
 	parseFlags()
 
 	if err := run(); err != nil {
@@ -24,8 +24,14 @@ func run() error {
 		return err
 	}
 
-	// создаём экземпляр приложения, пока без внешней зависимости хранилища сообщений
-	appInstance := newApp(nil)
+	// создаём соединение с СУБД PostgreSQL с помощью аргумента командной строки
+	conn, err := sql.Open("pgx", flagDatabaseURI)
+	if err != nil {
+		return err
+	}
+
+	// создаём экземпляр приложения, передавая реализацию хранилища pg в качестве внешней зависимости
+	appInstance := newApp(pg.NewStore(conn))
 
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
 	// обернём хендлер webhook в middleware с логированием и поддержкой gzip
--- go.mod
+++ go.mod
@@ -2,20 +2,26 @@ module github.com/Sorrowful-free/alice-skill
 
 go 1.24.5
 
-require github.com/stretchr/testify v1.10.0
+require (
+	github.com/golang/mock v1.6.0
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
-	github.com/golang/mock v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/tools v0.36.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-resty/resty/v2 v2.16.5
+	github.com/jackc/pgx/v5 v5.7.6
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/zap v1.27.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
